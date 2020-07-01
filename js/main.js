86450ccce5d5c78f1656a787d6f4e452fbb6f0aa
lucio <luciochen79@gmail.com>
2020-07-01 11:38:20 -0700

    Create pop up for "add to cart button" (homepage yet)
    Once click "add to cart", it shows the pop up window to inform users. From the pop up, you can click "view cart" to go to cart page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 // js for shopping cart button
-document.getElementById('cart-btn').addEventListener('click',
-    function(){
-    document.querySelector('.cart').style.display= 'block';
-});
+// document.getElementById('cart-btn').addEventListener('click',
+//     function(){
+//     document.querySelector('.cart').style.display= 'block';
+// });
 
-document.querySelector('.close-cart').addEventListener('click',
-    function(){
-    document.querySelector('.cart').style.display= 'none';
-});
+// document.querySelector('.close-cart').addEventListener('click',
+//     function(){
+//     document.querySelector('.cart').style.display= 'none';
+// });
 
 
 
@@ -16,3 +16,43 @@ document.querySelector('.close-cart').addEventListener('click',
 //     function(){
 //     document.querySelector('.bg-modal').style.display= 'none';
 // });
+
+
+// address page js
+// var checkbox= document.getElementsByClassName('address-checkbox');
+// console.log(checkbox);
+
+// for(var i=0; i<checkbox.length;i++){
+//     this.addEventListener('click', 
+//     function(e){
+//         e.target.parentNode.classList.toggle('selected-block');
+//         console.log("click");
+//     });
+// }
+
+
+
+
+// js for pop up when clicking "add to cart"
+
+//close pop up
+var closeBtn= document.querySelector('#close');
+var confirmBtn= document.querySelector('#confirm-btn');
+
+closeBtn.addEventListener('click', 
+    function(){
+        document.querySelector('.cart-popup').style.display= 'none';
+});
+confirmBtn.addEventListener('click', 
+    function(){
+        document.querySelector('.cart-popup').style.display= 'none';
+});
+
+// open pop up
+var addCartBtn= document.getElementsByClassName('add-to-cart');
+for(var i=0; i<addCartBtn.length; i++){
+    addCartBtn[i].addEventListener('click', function(){
+        document.querySelector('.cart-popup').style.display= 'flex';
+    });
+}
+
